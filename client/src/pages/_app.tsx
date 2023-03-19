23c5f25e1b941d965fafbe5457fa54b2485a988d
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-03-19 15:49:31 +0530

    feat: created custom hook to handle dark theme and color theme of app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,23 @@
+import { ThemeContextProvider } from '@/hooks/useTheme'
 import '@/styles/globals.css'
+import { NextPage } from 'next'
+import { ReactElement, ReactNode } from 'react'
+
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page): ReactNode => page)
+  return (
+    <ThemeContextProvider>
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeContextProvider>
+  )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'main-primary': 'rgb(var(--main-primary) / <alpha-value>)',
+        'main-secondary': 'rgb(var(--main-secondary) / <alpha-value>)',
+        'main-background': 'rgb(var(--main-background) / <alpha-value>)',
+        'light-background': 'rgb(var(--light-background) / <alpha-value>)',
+        'main-search-background':
+          'rgb(var(--main-search-background) / <alpha-value>)',
+        'main-sidebar-background':
+          'rgb(var(--main-sidebar-background) / <alpha-value>)',
+        'main-accent': 'rgb(var(--main-accent) / <alpha-value>)',
+        'accent-red': 'rgb(var(--accent-red) / <alpha-value>)',
+        'accent-blue': 'rgb(var(--accent-blue) / <alpha-value>)',
+        'accent-pink': 'rgb(var(--accent-pink) / <alpha-value>)',
+        'accent-purple': 'rgb(var(--accent-purple) / <alpha-value>)',
+        'accent-orange': 'rgb(var(--accent-orange) / <alpha-value>)',
+        'accent-green': 'rgb(var(--accent-green) / <alpha-value>)',
+        'dark-primary': '#E7E9EA',
+        'light-primary': '#F8FAFF',
+        'light-secondary': '#F0F4FA',
+        dark: '#212B36',
+        'dark-secondary': '#161C24',
+        'light-border': '#EFF3F4',
+        'light-line-reply': '#CFD9DE',
+        'twitter-icon': '#D6D9DB',
+        'image-preview-hover': '#272C30',
+      },
+      boxShadow: {
+        'main-shadow': '0 35px 60px -15px rgba(var(--main-accent))',
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
