7e6a1a6a8a02684fb29b603a29786147ad7d3ff7
mschem <mike.schem@stringking.com>
2023-03-19 21:57:06 -0700

    django messages basics

--- server/django/message/admin.py
+++ server/django/message/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Message
 
 
+@admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ("preview", "to", "sender", "text")
 
--- server/django/message/models.py
+++ server/django/message/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 
 class Message(models.Model):
     text = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    to = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="to")
-    sender = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="sender")
+    to = models.ForeignKey(User, on_delete=models.CASCADE, related_name="to")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
 
     def __str__(self):
         return self.text
